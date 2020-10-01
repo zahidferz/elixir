7dd27c4d0e6abce4db80703b0facf6faa6a2e08a
ZahidFernandez <zahid.ferz@gmail.com>
2020-10-01 15:40:59 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChainBusinessManagement.MixProject do
   def project do
     [
       app: :chain_business_management,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
